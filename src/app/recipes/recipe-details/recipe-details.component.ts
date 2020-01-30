18942347495543b8fb6f00821f6eb49949cc139c
Inkant Awasthi <10158214+tim1016@users.noreply.github.com>
2020-01-30 09:27:02 -0600

    Added dropdown menu

--- src/app/recipes/recipe-details/recipe-details.component.ts
+++ src/app/recipes/recipe-details/recipe-details.component.ts
@@ -8,6 +8,7 @@ import { Recipe } from "../../shared/recipes.model";
 })
 export class RecipeDetailsComponent implements OnInit {
   @Input() recipe: Recipe;
+  showDropdown = false;
   constructor() {}
 
   ngOnInit() {}
